8bde5c447d2875519380f6a243e2649017e01237
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-03-02 17:47:20 +0300

    added roundLogic function to compare moves between players and add score to the winner.

--- rps.js
+++ rps.js
@@ -9,8 +9,42 @@ function player(name) {
 };
 
 const player1 = player("james");
-
 const player2 = player("judy");
+
+player1.addMove("rock");
+player2.addMove("paper");
+
+
+function roundLogic() {
+    if ( player1.getCurrentMove() === player2.getCurrentMove()) {
+        console.log("its a tie");
+
+    } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "scissors") {
+        console.log("player 1 wins");
+        player1.addScore();
+    
+    } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "rock") {
+        console.log("player 2 wins");
+        player2.addScore();
+    
+    } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "rock") {
+        console.log("player 1 wins")
+        player1.addScore();
+    
+    } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "paper") {
+        console.log("player 2 wins");
+        player2.addScore();
+    
+    } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "paper") {
+        console.log("player 1 wins");
+        player1.addScore();
+    
+    } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "scissors") {
+        console.log("player 2 wins");
+        player2.addScore();
+    };
+};
+
 
 
 
