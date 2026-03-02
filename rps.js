9b90cc0ba42397785c7e213729ece4dd5e4e039e
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-03-02 17:08:19 +0300

    creeated player function factory.

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
+function player(name) {
+    let currentMove = "";
+    let currentScore = 0;
+    const getScore = () => currentScore;
+    const addScore = () => { currentScore++;};
+    const addMove = (move) => {currentMove = move;};
+    const getCurrentMove = () => currentMove;
+    return {name, getCurrentMove, getScore, addScore, addMove};
+};
+
+const player1 = player("james");
+
+const player2 = player("judy");
+
+
+
 
