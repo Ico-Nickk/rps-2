c0c75be0d10ddcf1b7229fbc26e846f6576354ae
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-03-03 10:53:54 +0300

    turned roundlogic into an IIFE module pattern. created a getwinmessage method to display the winner of the round.

--- rps.js
+++ rps.js
@@ -15,35 +15,50 @@ player1.addMove("rock");
 player2.addMove("paper");
 
 
-function roundLogic() {
-    if ( player1.getCurrentMove() === player2.getCurrentMove()) {
-        console.log("its a tie");
+const roundLogic = (() => {
+    let winMessage;
+    let roundsPlayed = 0;
+    const getWinMessage = () => {
+        if ( player1.getCurrentMove() === player2.getCurrentMove()) {
+            return winMessage = "its a tie";
+    
+        } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "scissors") {
+            return winMessage = "player 1 wins";
+            player1.addScore();
+            roundsPlayed++;
+            
+        } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "rock") {
+            return winMessage = "player 2 wins";
+            player2.addScore();
+            roundsPlayed++;
+        
+        } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "rock") {
+            return winMessage = "player 1 wins"
+            player1.addScore();
+            roundsPlayed++;
+        
+        } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "paper") {
+            return winMessage = "player 2 wins";
+            player2.addScore();
+            roundsPlayed++;
+        
+        } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "paper") {
+            return winMessage = "player 1 wins";
+            player1.addScore();
+            roundsPlayed++;
+        
+        } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "scissors") {
+            return winMessage = "player 2 wins";
+            player2.addScore();
+            roundsPlayed++;
+        };
+    }
 
-    } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "scissors") {
-        console.log("player 1 wins");
-        player1.addScore();
-    
-    } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "rock") {
-        console.log("player 2 wins");
-        player2.addScore();
-    
-    } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "rock") {
-        console.log("player 1 wins")
-        player1.addScore();
-    
-    } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "paper") {
-        console.log("player 2 wins");
-        player2.addScore();
-    
-    } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "paper") {
-        console.log("player 1 wins");
-        player1.addScore();
-    
-    } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "scissors") {
-        console.log("player 2 wins");
-        player2.addScore();
+    const winner = () => {
+        
     };
-};
+    return {getWinMessage};
+})();
 
 
 
