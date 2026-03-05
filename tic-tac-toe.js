45fdbda2e60720c97ad2d8cdf7a5d230e7cabb89
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-03-05 10:59:08 +0300

    removed round logic code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,60 +5,56 @@ function player(name) {
     const addScore = () => { currentScore++;};
     const addMove = (move) => {currentMove = move;};
     const getCurrentMove = () => currentMove;
-    return {name, getCurrentMove, getScore, addScore, addMove};
+    const resetScore = () => currentScore = 0;
+    return {name, getCurrentMove, getScore, addScore, addMove, resetScore};
 };
 
 const player1 = player("james");
 const player2 = player("judy");
 
+
+const gameOver = (() => {
+    let winMessage;
+    let champPlayer;
+    let champScore;
+    let looserScore;
+    const getChampPlayer = () => champPlayer;
+    const getChampScore = () => champScore;
+    const getLooserScore = () => looserScore;
+    const getWinMessage = () => winMessage; 
+    const winner = () => {
+        console.log("runnnig");
+        if (player1.getScore() > player2.getScore()) {
+            champPlayer = player1.name;
+            champScore = player1.getScore();
+            looserScore = player2.getScore();
+            winMessage = `${champPlayer} wins the game! ${champScore} to ${looserScore} `;
+            return 
+        } else if (player2.getScore() < player1.getScore()) {
+            champPlayer = player2.name;
+            champScore = player2.getScore();
+            looserScore = player1.getScore();
+            console.log(winMessage);
+            return winMessage = `${champPlayer} wins score is ${champScore} to ${looserScore} `;
+        } else if (player1.getScore() === player2.getScore()) {
+            winMessage = `its a tie score is ${player1.getScore()} to ${player2.getScore()} `;
+        };
+    };
+
+    const resetGame = () => {
+        roundLogic.resetRounds();
+        player1.resetScore();
+        player2.resetScore();
+    };
+
+    return {winner, getChampPlayer, getChampScore, getLooserScore, resetGame, getWinMessage};
+
+})();
+
 player1.addMove("rock");
 player2.addMove("paper");
-
-
-const roundLogic = (() => {
-    let winMessage;
-    let roundsPlayed = 0;
-    const getWinMessage = () => {
-        if ( player1.getCurrentMove() === player2.getCurrentMove()) {
-            return winMessage = "its a tie";
-    
-        } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "scissors") {
-            return winMessage = "player 1 wins";
-            player1.addScore();
-            roundsPlayed++;
-            
-        } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "rock") {
-            return winMessage = "player 2 wins";
-            player2.addScore();
-            roundsPlayed++;
-        
-        } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "rock") {
-            return winMessage = "player 1 wins"
-            player1.addScore();
-            roundsPlayed++;
-        
-        } else if (player1.getCurrentMove() === "rock" && player2.getCurrentMove() === "paper") {
-            return winMessage = "player 2 wins";
-            player2.addScore();
-            roundsPlayed++;
-        
-        } else if (player1.getCurrentMove() === "scissors" && player2.getCurrentMove() === "paper") {
-            return winMessage = "player 1 wins";
-            player1.addScore();
-            roundsPlayed++;
-        
-        } else if (player1.getCurrentMove() === "paper" && player2.getCurrentMove() === "scissors") {
-            return winMessage = "player 2 wins";
-            player2.addScore();
-            roundsPlayed++;
-        };
-    }
-
-    const winner = () => {
-        
-    };
-    return {getWinMessage};
-})();
+roundLogic.playRound();
+gameOver.winner();
 
 
 
