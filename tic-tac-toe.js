c7cb2df10c93e5582699c39f6b2cbbf19d92adfe
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-03-05 11:00:44 +0300

    removed gameover function that isnt for TTT game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -13,48 +13,6 @@ const player1 = player("james");
 const player2 = player("judy");
 
 
-const gameOver = (() => {
-    let winMessage;
-    let champPlayer;
-    let champScore;
-    let looserScore;
-    const getChampPlayer = () => champPlayer;
-    const getChampScore = () => champScore;
-    const getLooserScore = () => looserScore;
-    const getWinMessage = () => winMessage; 
-    const winner = () => {
-        console.log("runnnig");
-        if (player1.getScore() > player2.getScore()) {
-            champPlayer = player1.name;
-            champScore = player1.getScore();
-            looserScore = player2.getScore();
-            winMessage = `${champPlayer} wins the game! ${champScore} to ${looserScore} `;
-            return 
-        } else if (player2.getScore() < player1.getScore()) {
-            champPlayer = player2.name;
-            champScore = player2.getScore();
-            looserScore = player1.getScore();
-            console.log(winMessage);
-            return winMessage = `${champPlayer} wins score is ${champScore} to ${looserScore} `;
-        } else if (player1.getScore() === player2.getScore()) {
-            winMessage = `its a tie score is ${player1.getScore()} to ${player2.getScore()} `;
-        };
-    };
-
-    const resetGame = () => {
-        roundLogic.resetRounds();
-        player1.resetScore();
-        player2.resetScore();
-    };
-
-    return {winner, getChampPlayer, getChampScore, getLooserScore, resetGame, getWinMessage};
-
-})();
-
-player1.addMove("rock");
-player2.addMove("paper");
-roundLogic.playRound();
-gameOver.winner();
 
 
 
