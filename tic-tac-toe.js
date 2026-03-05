ff8b24ff06ebf5d7ac24bb063161634338c435dc
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-03-05 16:32:29 +0300

    created gameGrid and gridSquares objects and methods to set and get their values

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -12,6 +12,69 @@ function player(name) {
 const player1 = player("james");
 const player2 = player("judy");
 
+const gameBoard = (()=>{
+    
+    let gridSquares = {
+        a1: "",
+        a2: "a2",
+        a3: "a3",
+        b1: "b1",
+        b2: "b2",
+        b3: "b3",
+        c1: "c1",
+        c2: "c2",
+        c3: "c3",
+    };
+
+    let gameGrid = {
+        get colA(){ 
+            return [gridSquares.a1, gridSquares.a2, gridSquares.a3];
+        },
+
+        get colB(){
+            return [gridSquares.b1, gridSquares.b2, gridSquares.b3];
+        },
+
+        get colC(){
+            return [gridSquares.c1, gridSquares.c2, gridSquares.c3];
+        },
+
+        get row1(){
+            return [gridSquares.a1, gridSquares.b1, gridSquares.c1];
+        },
+
+        get row2(){
+            return [gridSquares.a2, gridSquares.b2, gridSquares.c2];
+        },
+
+        get row3(){
+            return [gridSquares.a3, gridSquares.b3, gridSquares.c3];
+        },
+
+        get diagonal1(){
+            return [gridSquares.a3, gridSquares.b2, gridSquares.c1];
+        },
+
+        get diagonal2(){
+            return [gridSquares.a1, gridSquares.b2, gridSquares.c3];
+        },
+
+    };
+
+    const getGridSquareVal = (square) => gridSquares[square];
+
+    const getBoardSection = (section) => gameGrid[section];
+
+    const showGridSquare = () => gridSquares;
+
+    const addSymbol = (square, sign) => {
+        gridSquares[square] = sign;
+    };
+
+    return {addSymbol, getGridSquareVal, getBoardSection, showGridSquare};
+})();
+
+
 
 
 
